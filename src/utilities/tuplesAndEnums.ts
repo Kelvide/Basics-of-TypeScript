a540a962735e3997c5da9a0c42e8583e5c339d1f
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-07-02 11:03:24 -0700

    update: Optional parameters in a tuple and readonly parameter

--- src/utilities/tuplesAndEnums.ts
+++ src/utilities/tuplesAndEnums.ts
@@ -11,5 +11,13 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 // A tuple is an array with a fixed number of elements, where each element has a specific type.
 // Tuples allow you to express an array where the type of a fixed number of elements is known.
-let tuple: [string, number, boolean] = ["example", 42, true];
+let tuple: readonly [string, number, boolean] = ["example", 42, true];
+
+// Note: Using tuple.push adds elements to the tuple, converting it to a regular array and bypassing the type restrictions defined for the tuple. To prevent that, we use the readonly property on the tuple.
+// tuple.push("Kelvin")
+
 console.log(tuple);
+
+// Set optional parameter in a tuple
+let student: readonly [string, number?] = ['Kelvin']
+console.log(student);
