b6c18855c24842d89f8a7fc2756ecb8bbfeee9dc
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-07-01 17:53:03 -0700

    update: Function for distinguishing between employees and managers

--- src/utilities/aliasAndInterface.ts
+++ src/utilities/aliasAndInterface.ts
@@ -15,6 +15,7 @@ type StringAlias = string;
 let greeting: StringAlias = "Hello, TypeScript!";
 console.log(greeting);
 
+// A better use case of an Alias
 type User = { name: string, id: number, isActive: boolean }
 
 const Liv: User = {
@@ -24,8 +25,29 @@ const Liv: User = {
 }
 
 function createUser(user: User): User {
-    console.log(`Hello, there ${user.name.toUpperCase()} !!!`)
+    console.log(`Hello, there ${user.name.toUpperCase()}!!!`)
     return user;
 }
 
 createUser(Liv)
+
+// Function for distinguishing between employees and managers
+type Employee = { name: string, id: number, dept: string }
+type Manager = { name: string, id: number, employees: Employee[] }
+
+type Staff = Employee | Manager
+
+function printStaffDetails(staff: Staff): void {
+    if ('employees' in staff) {
+        console.log(`${staff.name} is a manager for the ${staff.employees.length} employees`)
+    } else {
+        console.log(`${staff.name} is an employee in ${staff.dept} depertment`)
+    }
+}
+
+const favour: Employee = { id: 1, name: "Favour", dept: "QA" }
+const steve: Employee = { id: 1, name: "Steve", dept: "HR" }
+const Kelvin: Manager = { id: 1, name: "Favour", employees: [favour, steve] }
+
+printStaffDetails(steve)
+printStaffDetails(Kelvin)
