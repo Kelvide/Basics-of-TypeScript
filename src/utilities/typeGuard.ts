5e1b5a433bceba4c3e77a39c648f98f67179bc54
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-07-03 15:49:49 -0700

    update: Type Guard page added

---
+++ src/utilities/typeGuard.ts
@@ -0,0 +1,10 @@
+import '../style.css';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>TYPE GUARD</h1>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
