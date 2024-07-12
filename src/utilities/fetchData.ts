fa3ffe31e0c6d10518c9628517e163b49bc6e6bd
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-07-12 02:46:08 -0700

    update: used zod for type vaidation and schema declaration

--- src/utilities/fetchData.ts
+++ src/utilities/fetchData.ts
@@ -11,6 +11,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 // FETCH DATA
 // Typically axios and React Query (or both 🚀🚀🚀)
+import { z } from 'zod';
 const url = 'https://www.course-api.com/react-tours-project';
 
 // Define a type for the data you're fetching.
@@ -47,5 +48,51 @@ fetchData(url)
 
 const tours: Tour[] = await fetchData(url);
 tours.map((tour) => {
+    console.log(tour.name);
+});
+
+
+
+// ZOD
+// npm install zod
+// Using ZOD for type vaidation and schema declaration
+// Zod is a TypeScript-first schema declaration and validation library. I'm using the term "schema" to broadly refer to any data type, from a simple string to a complex nested object.
+const tourSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    info: z.string(),
+    image: z.string(),
+    price: z.string(),
+    something: z.string()
+});
+
+// extract the inferred type
+type ZTour = z.infer<typeof tourSchema>;
+
+async function zFetchData(url: string): Promise<ZTour[]> {
+    try {
+        const response = await fetch(url);
+
+        // Check if the request was successful.
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const rawData: ZTour[] = await response.json();
+        const result = tourSchema.array().safeParse(rawData);
+        if (!result.success) {
+            throw new Error(`Invalid data: ${result.error}`);
+        }
+        return result.data;
+    } catch (error) {
+        const errMsg = error instanceof Error ? error.message : 'there was an error...';
+        console.log(errMsg);
+        // throw error;
+        return [];
+    }
+}
+
+const ztours = await zFetchData(url);
+ztours.map((tour) => {
     console.log(tour.name);
 });
